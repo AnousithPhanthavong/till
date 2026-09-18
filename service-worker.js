ce7d52e2dbe427ef4109f017b184cfb8be439ac6
AnousithPhanthavong <162779355+AnousithPhanthavong@users.noreply.github.com>
2026-09-18 10:25:54 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Keeps a copy of the till on the iPad so it opens with no internet.
    Change CACHE when you send a new version of the files. */
 
-var CACHE = 'till-41';
+var CACHE = 'till-42';
 
 var NAV_WAIT_MS = 4000;
 
